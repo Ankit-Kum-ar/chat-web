92b76f7a53f173bdaf9369e5821dda080a079766
Ankit Kumar <ankitcode2511@gmail.com>
2025-05-05 12:38:01 +0530

    Update cookie options to enable cross-site requests with sameSite set to 'None' and secure set to true

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (userId, res) => {
     });
     res.cookie('token', token, {
         httpOnly: true,
-        // sameSite: 'Strict',
-        // secure: process.env.NODE_ENV !== 'development', // Set to true if using HTTPS
+        sameSite: 'None',
+        secure: true, // Required for cross-site requests
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
     return token;
